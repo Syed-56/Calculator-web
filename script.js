428842f92b2bb17764a7809ce493344084366614
Syed-56 <ssa.56.sultan@gmail.com>
2025-06-28 05:33:15 +0500

    Commented Workflow of Script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+//first get the div of display
+//now when a div is clicked, its content is written in the display div
+//first and second clicked div's content is passed to the function wrt operator, if + then sum() etc
+//the sum is returned and stored in the final output but not printed, bcz more operands and number can be inputted
+//when = button is clicked , computed sum is generated and previous content on display div is overriden
